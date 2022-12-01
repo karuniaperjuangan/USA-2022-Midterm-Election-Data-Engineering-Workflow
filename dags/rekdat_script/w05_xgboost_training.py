4d51528f0e52728da448e43a23d930d943075fcc
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2022-12-01 09:35:27 +0700

    revise w05, add notebook

--- dags/rekdat_script/w05_xgboost_training.py
+++ dags/rekdat_script/w05_xgboost_training.py
@@ -3,15 +3,21 @@ import pandas as pd
 from sklearn import datasets
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+
 
 def train_xgboost_model():
+    le = LabelEncoder()
+
     df = pd.read_csv("./output/election_summary.csv")
     df = df.dropna()
     
     X = df.drop(["District","Winners"],axis=1)
     y= df["Winners"]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    
+    y_train = le.fit_transform(y_train)
+    y_test = le.transform(y_test)
     model = xgb.XGBClassifier()
     model.fit(X_train, y_train)
     
